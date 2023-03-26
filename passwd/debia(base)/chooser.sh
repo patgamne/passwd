67e10cd0cd732d67ce2a1db772b628cf6325a299
patgamne <mr.patrick@pattymcgee.ca>
2023-03-26 21:54:11 +0000

    first commit

---
+++ passwd/debia(base)/chooser.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Welcome to the password encrypter/decrypter program!"
+echo "Please choose an option:"
+echo "1. Encrypt a password"
+echo "2. Decrypt a password"
+read choice
+
+if [ $choice -eq 1 ]
+then
+    python3 en.py
+elif [ $choice -eq 2 ]
+then
+    python3 de.py
+else
+    echo "Invalid choice. Please enter 1 or 2."
+fi
